405c047478a5d4357789df1705e6515a18e814e3
Elie Azzi <elie7777azzi@gmail.com>
2024-10-08 17:05:47 +0300

    fix: chains

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -3,14 +3,7 @@
 import { wagmiAdapter, projectId } from "@/config";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createAppKit } from "@reown/appkit/react";
-import {
-  mainnet,
-  arbitrum,
-  avalanche,
-  base,
-  optimism,
-  polygon,
-} from "@reown/appkit/networks";
+import { mainnet, base } from "@reown/appkit/networks";
 import React, { type ReactNode } from "react";
 import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
 
@@ -33,7 +26,7 @@ const metadata = {
 createAppKit({
   adapters: [wagmiAdapter],
   projectId,
-  networks: [mainnet, arbitrum, avalanche, base, optimism, polygon],
+  networks: [base],
   defaultNetwork: mainnet,
   metadata: metadata,
   features: {
